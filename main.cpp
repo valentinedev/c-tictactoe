00cd07d8e0765692a4dbc87924223e0567266c8a
valentinedev <exkuikui@gmail.com>
2017-06-30 20:13:01 -0700

    Add files via upload
    you now choose a square 1-9 instead of manually entering coordinates

--- main.cpp
+++ main.cpp
@@ -18,63 +18,73 @@ int main()
         while(1)
             {
             //Player 1
-            char pmx[2];
-            char pmy[2];
+            char pm[2];
             int pmx2;
             int pmy2;
-            cout<<"Player 1 X: ";
+            cout<<"Player 1: ";
             while(1)
             {
-                cin>>pmx;
+                cin>>pm;
                 cin.ignore();
-                if (strcmp(pmx,"0")==0||strcmp(pmx,"1")==0||strcmp(pmx,"2")==0)
+                if (strcmp(pm,"1")==0||strcmp(pm,"2")==0||strcmp(pm,"3")==0||strcmp(pm,"4")==0||strcmp(pm,"5")==0||strcmp(pm,"6")==0||strcmp(pm,"7")==0||strcmp(pm,"8")==0||strcmp(pm,"9")==0)
                 {
-                    if(strcmp(pmx,"0")==0)
+                    if(strcmp(pm,"1")==0)
                     {
                         pmx2=0;
-                    }
-                    else if(strcmp(pmx,"1")==0)
-                    {
-                        pmx2=1;
-                    }
-                    else if(strcmp(pmx,"2")==0)
-                    {
-                        pmx2=2;
-                    }
-                    break;
-                }
-                cout<<"Invalid Input\nPlayer 1 X: ";
-            }
-            cout<<"Player 1 Y: ";
-            while(1)
-            {
-                cin>>pmy;
-                cin.ignore();
-                if (strcmp(pmy,"0")==0||strcmp(pmy,"1")==0||strcmp(pmy,"2")==0)
-                {
-                    if(strcmp(pmy,"0")==0)
-                    {
                         pmy2=0;
                     }
-                    else if(strcmp(pmy,"1")==0)
+                    else if(strcmp(pm,"2")==0)
                     {
+                        pmx2=1;
+                        pmy2=0;
+                    }
+                    else if(strcmp(pm,"3")==0)
+                    {
+                        pmx2=2;
+                        pmy2=0;
+                    }
+                    else if(strcmp(pm,"4")==0)
+                    {
+                        pmx2=0;
+                        pmy2=1;
+
+                    }
+                    else if(strcmp(pm,"5")==0)
+                    {
+                        pmx2=1;
                         pmy2=1;
                     }
-                    else if(strcmp(pmy,"2")==0)
+                    else if(strcmp(pm,"6")==0)
                     {
+                        pmx2=2;
+                        pmy2=1;
+                    }
+                    else if(strcmp(pm,"7")==0)
+                    {
+                        pmx2=0;
+                        pmy2=2;
+                    }
+                    else if(strcmp(pm,"8")==0)
+                    {
+                        pmx2=1;
+                        pmy2=2;
+                    }
+                    else if(strcmp(pm,"9")==0)
+                    {
+                        pmx2=2;
                         pmy2=2;
                     }
                     break;
                 }
-                cout<<"Invalid Input\nPlayer 1 Y: ";
+                cout<<"Invalid Input\nPlayer 1: ";
             }
             if (exarray[pmy2][pmx2] == 1 || exarray[pmy2][pmx2] == 2)
             {
                 cout<<"Invalid coordinate\n";
                 continue;
             }
-            cout<<"Placed a 1 at "<<pmx2<<","<<pmy2<<"\n";
             exarray[pmy2][pmx2] = 1;
+            system("CLS");
             cout<<"["<<exarray[0][0]<<"]"<<"["<<exarray[0][1]<<"]"<<"["<<exarray[0][2]<<"]\n"<<"["<<exarray[1][0]<<"]"<<"["<<exarray[1][1]<<"]"<<"["<<exarray[1][2]<<"]\n"<<"["<<exarray[2][0]<<"]"<<"["<<exarray[2][1]<<"]"<<"["<<exarray[2][2]<<"]\n";
             if ((exarray[0][0]==1&&exarray[0][1]==1&&exarray[0][2]==1)||(exarray[1][0]==1&&exarray[1][1]==1&&exarray[1][2]==1)||(exarray[2][0]==1&&exarray[2][1]==1&&exarray[2][2]==1)||(exarray[0][0]==1&&exarray[1][0]==1&&exarray[2][0]==1)||(exarray[0][1]==1&&exarray[1][1]==1&&exarray[2][1]==1)||(exarray[0][2]==1&&exarray[1][2]==1&&exarray[2][2]==1)||(exarray[0][0]==1&&exarray[1][1]==1&&exarray[2][2]==1)||(exarray[2][0]==1&&exarray[1][1]==1&&exarray[0][2]==1))
             {
@@ -97,66 +107,73 @@ int main()
         while(1)
         {
             //Player 2
-            char p2mx[2];
-            char p2my[2];
+            char p2m[2];
             int p2mx2;
             int p2my2;
-            cout<<"Player 2 X: ";
+            cout<<"Player 2: ";
             while(1)
             {
-                cin>>p2mx;
+                cin>>p2m;
                 cin.ignore();
-                if (strcmp(p2mx,"0")==0||strcmp(p2mx,"1")==0||strcmp(p2mx,"2")==0)
+                if (strcmp(p2m,"1")==0||strcmp(p2m,"2")==0||strcmp(p2m,"3")==0||strcmp(p2m,"4")==0||strcmp(p2m,"5")==0||strcmp(p2m,"6")==0||strcmp(p2m,"7")==0||strcmp(p2m,"8")==0||strcmp(p2m,"9")==0)
                 {
-                    if(strcmp(p2mx,"0")==0)
+                    if(strcmp(p2m,"1")==0)
                     {
                         p2mx2=0;
-                    }
-                    else if(strcmp(p2mx,"1")==0)
-                    {
-                        p2mx2=1;
-                    }
-                    else if(strcmp(p2mx,"2")==0)
-                    {
-                        p2mx2=2;
-                    }
-                    break;
-                }
-                cout<<"Invalid Input\nPlayer 2 X: ";
-            }
-            cout<<"Player 2 Y: ";
-            while(1)
-            {
-                cin>>p2my;
-                cin.ignore();
-                if (strcmp(p2my,"0")==0||strcmp(p2my,"1")==0||strcmp(p2my,"2")==0)
-                {
-                    if(strcmp(p2my,"0")==0)
-                    {
                         p2my2=0;
                     }
-                    else if(strcmp(p2my,"1")==0)
+                    else if(strcmp(p2m,"2")==0)
                     {
+                        p2mx2=1;
+                        p2my2=0;
+                    }
+                    else if(strcmp(p2m,"3")==0)
+                    {
+                        p2mx2=2;
+                        p2my2=0;
+                    }
+                    else if(strcmp(p2m,"4")==0)
+                    {
+                        p2mx2=0;
+                        p2my2=1;
+
+                    }
+                    else if(strcmp(p2m,"5")==0)
+                    {
+                        p2mx2=1;
                         p2my2=1;
                     }
-                    else if(strcmp(p2my,"2")==0)
+                    else if(strcmp(p2m,"6")==0)
                     {
-                             if (winval==1)
-        {
-            break;
-        }   p2my2=2;
+                        p2mx2=2;
+                        p2my2=1;
+                    }
+                    else if(strcmp(p2m,"7")==0)
+                    {
+                        p2mx2=0;
+                        p2my2=2;
+                    }
+                    else if(strcmp(p2m,"8")==0)
+                    {
+                        p2mx2=1;
+                        p2my2=2;
+                    }
+                    else if(strcmp(p2m,"9")==0)
+                    {
+                        p2mx2=2;
+                        p2my2=2;
                     }
                     break;
                 }
-                cout<<"Invalid Input\nPlayer 2 Y: ";
+                cout<<"Invalid Input\nPlayer 2: ";
             }
             if (exarray[p2my2][p2mx2] == 1 || exarray[p2my2][p2mx2] == 2)
             {
                 cout<<"Invalid coordinate\n";
                 continue;
             }
-            cout<<"Placed a 2 at "<<p2mx2<<","<<p2my2<<"\n";
             exarray[p2my2][p2mx2] = 2;
+            system("CLS");
             cout<<"["<<exarray[0][0]<<"]"<<"["<<exarray[0][1]<<"]"<<"["<<exarray[0][2]<<"]\n"<<"["<<exarray[1][0]<<"]"<<"["<<exarray[1][1]<<"]"<<"["<<exarray[1][2]<<"]\n"<<"["<<exarray[2][0]<<"]"<<"["<<exarray[2][1]<<"]"<<"["<<exarray[2][2]<<"]\n";
             if ((exarray[0][0]==2&&exarray[0][1]==2&&exarray[0][2]==2)||(exarray[1][0]==2&&exarray[1][1]==2&&exarray[1][2]==2)||(exarray[2][0]==2&&exarray[2][1]==2&&exarray[2][2]==2)||(exarray[0][0]==2&&exarray[1][0]==2&&exarray[2][0]==2)||(exarray[0][1]==2&&exarray[1][1]==2&&exarray[2][1]==2)||(exarray[0][2]==2&&exarray[1][2]==2&&exarray[2][2]==2)||(exarray[0][0]==2&&exarray[1][1]==2&&exarray[2][2]==2)||(exarray[2][0]==2&&exarray[1][1]==2&&exarray[0][2]==2))
             {
